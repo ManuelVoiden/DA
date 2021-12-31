5d05fe8d912c18b8d032c4dc31fe6cba49426679
ManuelVoiden <jmmb710@hotmail.com>
2021-12-31 04:26:00 -0500

    Inicial

---
+++ Python/Clase 11.py
@@ -0,0 +1,12 @@
+import math
+
+#En Python se comenta con el numeral y se acaba el comentario al pasar a la siguiente linea
+#En la clase 11 de el curso básico de Python enseña sobre la reconversión de variables con los siguientes comandos
+    #Input("") para pedirle al usuario que introduzca datos.
+    #int() con datos o variables dentro de parentesis para convertirlo en número entero.
+    #str() para convertir números tanto decimales como enteros a strings.
+    
+#El siguiente es un ejemplo de buenas practicas al escribir variables:
+
+num = int(input('Escribe un número: '))
+print(num)
---
+++ Python/Clase 13.py
@@ -0,0 +1,61 @@
+import math
+
+eur_usd = 1.13
+usd_eur = 0.88
+eur_cop = 4603.37
+cop_eur = 0.00022
+usd_cop = 4070
+cop_usd = 0.00025
+
+mon1 = 'EUR' 
+mon2 = 'USD' 
+mon3 = 'COP'
+
+valor_a_convertir = 0
+valor_final = 0
+
+print("Bienvenido a monedapp")
+print("Puedes convertir entre las siguientes monedas: EUR, USD y COP")
+print("")
+moneda_inicio = str(input('Que moneda quieres convertir: '))
+moneda_final = str(input('A que moneda quieres convertir: '))
+print("")
+
+if moneda_inicio == mon1 and moneda_final == mon2:
+    print(f'Convertiremos entre {mon1} y {mon2}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * eur_usd
+    print(f'{mon1} {valor_a_convertir} equivale a {mon2} {valor_final}')
+elif moneda_inicio == mon1 and moneda_final == mon3:
+    print(f'Convertiremos entre {mon1} y {mon3}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * eur_cop
+    print(f'{mon1} {valor_a_convertir} equivale a {mon3} {valor_final}')
+elif moneda_inicio == mon2 and moneda_final == mon3:
+    print(f'Convertiremos entre {mon2} y {mon3}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * usd_cop
+    print(f'{mon2} {valor_a_convertir} equivale a {mon3} {valor_final}')
+elif moneda_inicio == mon2 and moneda_final == mon1:
+    print(f'Convertiremos entre {mon2} y {mon1}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * usd_eur
+    print(f'{mon2} {valor_a_convertir} equivale a {mon1} {valor_final}')
+elif moneda_inicio == mon3 and moneda_final == mon1:
+    print(f'Convertiremos entre {mon3} y {mon1}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * cop_eur
+    print(f'{mon3} {valor_a_convertir} equivale a {mon1} {valor_final}')
+elif moneda_inicio == mon3 and moneda_final == mon2:
+    print(f'Convertiremos entre {mon3} y {mon2}')
+    valor_a_convertir = int(input('Inserta el valor a convertir: '))
+    valor_final = valor_a_convertir * cop_usd
+    print(f'{mon3} {valor_a_convertir} equivale a {mon2} {valor_final}')
+else:
+    print('ERROR, moneda no soportada por el programa')
+    
+
+print('¡Gracias por usar Monedapp!')
+    
+    
+    
