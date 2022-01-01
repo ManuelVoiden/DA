97aa0657966bad3b052f3c75a1bc51aa75c408be
ManuelVoiden <jmmb710@hotmail.com>
2021-12-31 23:12:15 -0500

    Crear primera funcion con def

---
+++ Python/Clase 16.py
@@ -0,0 +1,7 @@
+#Esta clase está dedicada a aprender sobre la creación de funciones
+#las funciones son utiles cuando se debe repetir un pedazo de codigo
+
+def texto_repetir():
+    print('Mensaje especial: ')
+    print('Estoy aprendiendo a usar funciones')
+    
