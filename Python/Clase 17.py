e84ab9facd4c4ce6f49ad38eb105e71a85a5630f
Juan Manuel Marin <manuelvoiden@gmail.com>
2022-01-13 20:17:06 -0500

    Created first functions exercise

---
+++ Python/Clase 17.py
@@ -0,0 +1,7 @@
+def suma(a, b):
+    print('Se imprimen dos numeros')
+    resultados = a+b
+    return resultados   #se usa return para poder extraer el valor de la suma de la función
+
+sumatoria = suma(1, 5)  #sumatoria es igual a el valor que returna suma, en este caso es el valor de resultados
+print(sumatoria) 
