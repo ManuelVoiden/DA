518b65100ce58df67326b484382e8e351fa03a03
Juan Manuel Marín <jmmb710@hotmail.com>
2022-02-02 11:57:59 -0500

    Working version 1.0

--- Python/Datacademy/Clase28.py
+++ Python/Datacademy/Clase28.py
@@ -1,23 +1,24 @@
 from telnetlib import theNULL
 
 
-def palindromo(frase):
+def palindromo(frase, frase_fix):
     if frase == frase[::-1]:
-        print(f'La frase {frase} es un palindromo!, al reves es {frase[::-1]}')
+        print(f'La frase {frase} es un palindromo!, al reves es {frase_fix[::-1]}')
     else:
-        print(f'Lo sentimos, la frase {frase} no es un palindromo, al reves es {frase[::-1]} ingresa otra frase y comprueba de nuevo.')
+        print(f'Lo sentimos, la frase {frase} no es un palindromo, al reves es {frase_fix[::-1]} ingresa otra frase y comprueba de nuevo.')
         
 def limpieza(frase):
     frase=frase.lower()
     frase=frase.strip()
     frase=frase.replace(' ','')
+    print(frase)
     return frase
         
 def run():
     print("Hola y bienvenido al comprobador de palindromos.")
     frase = str(input("Escribe la palabra o frase a la que quieras comprobar si es un palindromo: "))
-    limpieza(frase)
-    palindromo(frase)
+    frase_fix = limpieza(frase)
+    palindromo(frase, frase_fix)
     
 if __name__ == '__main__':
     run()
