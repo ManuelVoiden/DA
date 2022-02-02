b39f0f5365f39b6b4a04ff1aa77360ca32313052
Juan Manuel Marín <jmmb710@hotmail.com>
2022-02-02 10:32:43 -0500

    Deleted debug print

--- Python/Datacademy/DCademy1.py
+++ Python/Datacademy/DCademy1.py
@@ -19,9 +19,7 @@ class conversor_moneda:
             elif moneda_inicial != 'EUR': 
                   cantidad = ((cantidad/self.rates[moneda_inicial])*self.rates[moneda_final])
             print("Tu conversión está lista")
-            print(f'{moneda_inicial} {cantidad_inicial} equivalen a {moneda_final} {cantidad}')
-            print(self.rates[moneda_final])
-            print(self.rates[moneda_inicial])
+            print(f'{moneda_inicial} {cantidad_inicial} equivalen a {moneda_final} {cantidad}')s
             
 if __name__ == "__main__":
       url = str.__add__('http://data.fixer.io/api/latest?access_key=','4e6b538d016ea8c6948fdf52ec855fbd')
