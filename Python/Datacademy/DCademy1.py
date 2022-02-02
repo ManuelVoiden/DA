501edfd3b5928e0e1f8ce5d0cc68f92cc61ccf3d
Juan Manuel Marín <jmmb710@hotmail.com>
2022-02-02 08:28:17 -0500

    First incorporation of Exchange API

--- Python/Datacademy/DCademy1.py
+++ Python/Datacademy/DCademy1.py
@@ -2,17 +2,24 @@
 
 import math
 import json
+from unittest import result
+from urllib import response
 import requests
 
-from pprint import pprint
+print("Bienvenido a monedapp")
+print("""Puedes convertir entre las siguientes monedas: 
+-> EUR, USD, CAD, AUD, GBP, COP, CHF, MXN, ARS, CLP, PEN 
+      """)
+print("")
+moneda_inicio = str(input('Que moneda quieres convertir: '))
+moneda_final = str(input('A que moneda quieres convertir: '))
+cantidad = int(input('Inserta el valor a convertir: '))
+print("")
 
-apiFile= open("api.json")
 
-data.json.load(apiFile)
-api_key=data["API_KEY"]
-
-url="http://data.fixer.io/api/latest" +api_key
-
-dataURL= requests.get(url).text
-dataJSON= json.loads(dataURL)
-
+response = requests.get(f'https://api.exchangeratesapi.io/v1/latest?base={moneda_inicio}&symbols={moneda_final}')
+data = response.json()
+tasa_de_cambio = data['rates'][moneda_inicio]
+resultado = cantidad*tasa_de_cambio
+print("Tu conversión está lista")
+print(f'{moneda_inicio} {cantidad} equivalen a {moneda_final} {tasa_de_cambio}')
