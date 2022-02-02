269285f705f6e47779e84eeae013f937e5526a0b
Juan Manuel Marín <jmmb710@hotmail.com>
2022-02-02 10:13:37 -0500

    Need to fix currency ticker Upper and lower case

--- Python/Datacademy/DCademy1.py
+++ Python/Datacademy/DCademy1.py
@@ -16,10 +16,6 @@ moneda_final = str(input('A que moneda quieres convertir: '))
 cantidad = int(input('Inserta el valor a convertir: '))
 print("")
 
-url = str.__add__('http://data.fixer.io/api/latest?access_key=','4e6b538d016ea8c6948fdf52ec855fbd')
-c = conversor_moneda(url)
-c.conversor(moneda_inicial, moneda_final, cantidad)
-
 class conversor_moneda:
       rates: {}
       
@@ -30,10 +26,10 @@ class conversor_moneda:
       def conversor(self, moneda_inicial, moneda_final, cantidad):
             cantidad_inicial = cantidad
             if moneda_inicial != 'EUR':
-                  tasa_de_cambio = cantidad/self.rates[moneda_inicial]
+                  cantidad = cantidad/self.rates[moneda_inicial]
             print("Tu conversión está lista")
-            print(f'{moneda_inicial} {cantidad_inicial} equivalen a {moneda_final} {tasa_de_cambio}')
-tasa_de_cambio = data['rates'][moneda_inicial]
-resultado = cantidad*tasa_de_cambio
-print("Tu conversión está lista")
-print(f'{moneda_inicio} {cantidad} equivalen a {moneda_final} {tasa_de_cambio}')
+            print(f'{moneda_inicial} {cantidad_inicial} equivalen a {moneda_final} {cantidad}')
+            
+url = str.__add__('http://data.fixer.io/api/latest?access_key=','4e6b538d016ea8c6948fdf52ec855fbd')
+c = conversor_moneda(url)
+c.conversor(moneda_inicial, moneda_final, cantidad)
