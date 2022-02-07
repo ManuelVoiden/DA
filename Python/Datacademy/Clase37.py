bd9a6d4ed71743a6f52e3093805ffb74b82a7cd1
Juan Manuel Marín B <ju.marin@javeriana.edu.co>
2022-02-07 16:11:28 -0500

    Finishe class 37 file

---
+++ Python/Datacademy/Clase37.py
@@ -0,0 +1,8 @@
+#Introducción a las tuplas
+#Las tuplas son como las listas pero inmutables o estaticas, no se le pueden agregar elementos pero su ejecución es mucho mas rapida
+
+datos_usuario=('Juan', 'Marin', 22, 4.24)
+print(datos_usuario)
+
+#El resto de propiedades se mantienen por lo que podemos darle la vuelta sin problema
+datos_usuario[::-1]
