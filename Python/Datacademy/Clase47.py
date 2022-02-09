c02b7c9b17cd1e069fba7459868c1744e1c5c8cd
ManuelVoiden <jmmb710@hotmail.com>
2022-02-09 11:07:48 -0500

    Finished class 47 file

--- Python/Datacademy/Clase47.py
+++ Python/Datacademy/Clase47.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 df_estudiantes = pd.read_csv('studentsperformance_f29e92e5-811f-4cf9-9504-91644b0ecc35.csv')
 print(df_estudiantes)
+
+df_estudiantes.head()
+df_estudiantes.tail()
+df_estudiantes.sample(7)
+print(df_estudiantes.shape)
+df_estudiantes.describe()
