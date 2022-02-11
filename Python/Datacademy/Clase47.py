33c2eb8a735249421a72a80c0e9225a30b194d46
ManuelVoiden <jmmb710@hotmail.com>
2022-02-11 12:43:36 -0500

    Updated csv file name

--- Python/Datacademy/Clase47.py
+++ Python/Datacademy/Clase47.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_estudiantes = pd.read_csv('studentsperformance_f29e92e5-811f-4cf9-9504-91644b0ecc35.csv')
+df_estudiantes = pd.read_csv('studentsperformance.csv')
 print(df_estudiantes)
 
 df_estudiantes.head()
