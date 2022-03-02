0c204c8daa938f9b278eeabbd5c8a50a6ed24620
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 12:10:51 -0500

    Added extra variables to UberVan.py

--- TestUberPOO/UberVan.py
+++ TestUberPOO/UberVan.py
@@ -3,8 +3,12 @@ from car import Car
 class UberVan(Car):  #Se aplica Herencia en estos archivos
     brand = str
     model = str
+    typeCarAccepted = []
+    seatsMaterial = []
     
     def __init__(self, license, driver, brand, model):
         super.__init(license, driver) #Aqui se esta haciendo un __init__ anidado, se llaman los datos de license y driver desde Car, los otros dos datos son propios de el tipo de Uber
         self.brand = brand
         self.model = model
+        self.typeCarAccepted = typeCarAccepted
+        self.seatsMaterial = seatsMaterial
