2806b4eed7a2d305762f04ccbc41f08589e87408
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 12:06:29 -0500

    Filled up the UberX.py file with init annidated

---
+++ TestUberPOO/UberX.py
@@ -0,0 +1,10 @@
+from car import Car
+
+class UberX(Car):  #Se aplica Herencia en estos archivos
+    brand = str
+    model = str
+    
+    def __init__(self, license, driver, brand, model):
+        super.__init(license, driver) #Aqui se esta haciendo un __init__ anidado, se llaman los datos de license y driver desde Car, los otros dos datos son propios de el tipo de Uber
+        self.brand = brand
+        self.model = model
