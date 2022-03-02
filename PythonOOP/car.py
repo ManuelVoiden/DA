3a668c60844ea620aa571635105f317a601b2d5a
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 11:51:44 -0500

    Fixed account fields, one for every variable inside account.py

--- PythonOOP/car.py
+++ PythonOOP/car.py
@@ -3,7 +3,7 @@ from account import Account
 class Car:
     id = int
     license = str
-    driver = Account("","")   ##Se declara driver = Account ya que importamos el archivo account.py, esto permite vincular directamente ambos archivos
+    driver = Account("", "", "", "", "")   ##Se declara driver = Account ya que importamos el archivo account.py, esto permite vincular directamente ambos archivos
     passenger = int
     
     def __init__(self, license, driver):
