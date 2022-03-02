49bcdf0714bb784c5a6b4643ebcfa573de7b840b
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 10:54:27 -0500

    Adding cars to the Main via car.py

--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    
+    car = Car()
+    car.license = "PQR325"
+    car.driver = "Juan Manuel Marín Bedoya"
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "PAR377"
+    car2.driver = "Luis Mario Marín Bedoya"
+    print(vars(car2))
