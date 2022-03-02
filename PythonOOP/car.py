32c991fb96cc677f5c797f9b5432067764cc7461
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 10:59:44 -0500

    Updated car.py with constructor method and linked account.py

--- PythonOOP/car.py
+++ PythonOOP/car.py
@@ -1,6 +1,11 @@
+from account import Account
+
 class Car:
     id = int
     license = str
-    driver = str
+    driver = Account("","")   ##Se declara driver = Account ya que importamos el archivo account.py, esto permite vincular directamente ambos archivos
     passenger = int
     
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
