f85fdbfcdfd45173abb766dbcf22f59365894d11
ManuelVoiden <jmmb710@hotmail.com>
2022-03-02 11:52:09 -0500

    Fixed new form of creating data on main.py

--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -1,14 +1,20 @@
-from car import Car
+from car import Car  #Se debe importar los archivos de los que vamos a sacar datos
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
+
+    #car = Car()
+    #car.license = "PQR325"
+    #car.driver = "Juan Manuel Marín Bedoya"
+    #print(vars(car))
     
-    car = Car()
-    car.license = "PQR325"
-    car.driver = "Juan Manuel Marín Bedoya"
-    print(vars(car))
+    #car2 = Car()
+    #car2.license = "PAR377"
+    #car2.driver = "Luis Mario Marín Bedoya"
+    #print(vars(car2))
+        
+    car3 = Car("BTU908", Account(1, "Juliana Suarez Avila", 1005413227, "juliuwu@gmail.com", "ajiacoajiaco")) #Aqui se puede ver otra forma de introducir datos al objeto car
+    print(vars(car3))
+    print(vars(car3.driver))
     
-    car2 = Car()
-    car2.license = "PAR377"
-    car2.driver = "Luis Mario Marín Bedoya"
-    print(vars(car2))
