18638393424290f493f3747610ffdd50abd86872
ManuelVoiden <jmmb710@hotmail.com>
2022-03-21 23:22:53 -0500

    Added method for printing superdict

--- Python/Curso Intermedio/Clase7.py
+++ Python/Curso Intermedio/Clase7.py
@@ -5,15 +5,28 @@ def run():
     my_dict = {"firstname": "Juan Manuel", "lastname": "Marín"}
     
     #Una superlistas es una lista que tiene diccionarios dentro
-    
     super_list = [
         {"firstname": "Juan Manuel", "lastname": "Marín"},
         {"firstname": "Juliana", "lastname": "Suarez"},
         {"firstname": "Maria Jose", "lastname": "Marín"},
-        {"firstname": "Juan Miguel", "lastname": "Mejia"},
+        {"firstname": "Facundo", "lastname": "Garcia"},
         {"firstname": "Dua", "lastname": "Lipa"},
     ]
 
+    print(super_list)
+    
+    
+    #Un superdiccionario incluye multiple listas
+    super_dict = {
+        "numeros_positivos": [1, 2, 3, 4, 5],
+        "numeros_negativos": [-5, -4, -3, -2, -1],
+        "numeros_flotantes": [0.7, 7.9, 11.5, 0.22],
+    } 
+    
+    
+    #Ahora para imprimir los valores dentro del superdiccionario se usa un for con la terminación .items
+    for key, value in super_dict.items():
+        print(key, "-", value)
 
 if __name__ == '__main__':
     run()
