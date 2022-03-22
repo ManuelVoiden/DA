b470524fb9ac7f4969a3d4d991b951b1521d6ffa
ManuelVoiden <jmmb710@hotmail.com>
2022-03-21 23:14:24 -0500

    Added superlist

--- Python/Curso Intermedio/Clase7.py
+++ Python/Curso Intermedio/Clase7.py
@@ -3,6 +3,16 @@
 def run():
     my_list = [433, "Buenos Dias", False, 7.78]
     my_dict = {"firstname": "Juan Manuel", "lastname": "Marín"}
+    
+    #Una superlistas es una lista que tiene diccionarios dentro
+    
+    super_list = [
+        {"firstname": "Juan Manuel", "lastname": "Marín"},
+        {"firstname": "Juliana", "lastname": "Suarez"},
+        {"firstname": "Maria Jose", "lastname": "Marín"},
+        {"firstname": "Juan Miguel", "lastname": "Mejia"},
+        {"firstname": "Dua", "lastname": "Lipa"},
+    ]
 
 
 if __name__ == '__main__':
