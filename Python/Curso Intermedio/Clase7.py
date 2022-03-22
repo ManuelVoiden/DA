2d257585e9c271891895ad9a52c214919413066c
ManuelVoiden <jmmb710@hotmail.com>
2022-03-21 23:08:47 -0500

    Started class7

---
+++ Python/Curso Intermedio/Clase7.py
@@ -0,0 +1,9 @@
+#Listas y diccionarios
+
+def run():
+    my_list = [433, "Buenos Dias", False, 7.78]
+    my_dict = {"firstname": "Juan Manuel", "lastname": "Marín"}
+
+
+if __name__ == '__main__':
+    run()
