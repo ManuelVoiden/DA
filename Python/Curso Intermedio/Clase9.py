57acbcf9725c0414edf954636eb55cdf7950a5db
Juan Manuel Marín B <JMMB710@hotmail.com>
2022-03-30 12:07:02 -0500

    Created an automated filled dictionary

---
+++ Python/Curso Intermedio/Clase9.py
@@ -0,0 +1,12 @@
+def run():
+    mi_diccionario = {}
+    
+    for i in range(1, 100):
+        mi_diccionario[i] = i**3
+        
+    print(mi_diccionario)
+    
+    
+    
+if __name__ == '__main__':
+    run()   
