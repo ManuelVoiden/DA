f2e58cbc0fbe6a59f3af940fe021f05145202a94
Juan Manuel Marín B <JMMB710@hotmail.com>
2022-03-30 02:11:14 -0500

    New way of creating list with comprehensions

--- Python/Curso Intermedio/Clase8.py
+++ Python/Curso Intermedio/Clase8.py
@@ -9,8 +9,8 @@ def cuadrados():
         
     print(numeros_cuadrados)
     
-    
-    
+
+##Metodo tradicional sin list comprehensions    
 def cuadrados_divisibles():
     numeros_cuadrados_divisibles = []
     for i in range(1, maximo):
@@ -21,9 +21,20 @@ def cuadrados_divisibles():
     
     
     
+##Nuevo metodo con list comprehensions    
+def nuevo_cuadrados_divisibles():
+    nuevo_numeros_cuadrados_divisibles = [i**2 for i in range(1, maximo) if i%3 != 0]
+    ##Esto sigue la estructura de:
+    ##[elemento for elemento in iterable if condicion]
+    print(nuevo_numeros_cuadrados_divisibles)
+    
+    
+##Reto de la clase
+
     
     
 if __name__ == '__main__':
     cuadrados()
     cuadrados_divisibles()
+    nuevo_cuadrados_divisibles()
     
